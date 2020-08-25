cf74c75bb8c2e835529ba36eb5189efa13256848
elisasanches <60738579+elisasanches@users.noreply.github.com>
2020-08-25 00:01:00 -0300

    Desenvolvimento do App Tarefas
    Desenvolvendo mensagens de Validaçao

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public formLogin: FormGroup;
+
+  public mensagens_validacao = {
+    email: [
+      { tipo: 'required', mensagem: 'O campo E-mail é obrigatório!' },
+      { tipo: 'email', mensagem: 'E-mail inválido!' }
+    ],
+    senha: [
+      { tipo: 'required', mensagem: 'O campo senha é obrigatório!' },
+      { tipo: 'minLength', mensagem: 'A senha deve ter pelo menos 6 caracteres!'}
+    ]
+  };
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.formLogin = formBuilder.group ({
+      email: ['',Validators.compose([Validators.required, Validators.email])],
+      senha: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+    });
+  }
 
   ngOnInit() {
+  }
+
+  public login(){
+    if(this.formLogin.valid) {
+      console.log('formulário válido!');
+    } else {
+      console.log('formulário inválido!')
+    }
   }
 
 }
